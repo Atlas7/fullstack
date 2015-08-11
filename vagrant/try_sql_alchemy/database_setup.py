84f96ce112c544d5f0cb7ecefd93c6d83e4f08fe
Johnny <johnnychan0302@gmail.com>
2015-08-11 09:05:05 +0100

    correct database_setup.py

--- vagrant/try_sql_alchemy/database_setup.py
+++ vagrant/try_sql_alchemy/database_setup.py
@@ -1,6 +1,6 @@
-################
-## Config Begin
-################
+#######################
+# Config Begin
+#######################
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -10,28 +10,31 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-################
-## Class
-################
+#######################
+# Class
+#######################
 class Restaurant(Base):
-	__tablename__ = "restaurant"
+    __tablename__ = 'restaurant'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
 
 class MenuItem(Base):
-	__tablename__ = "menu_item"
+    __tablename__ = 'menu_item'
 
-	name = Column(String(80), nullable=False)
-	id = Column(Integer, primary_key=True)
-	description = Column(String(250))
-	price = Column(String(8))
-	course = Column(String(250))
-	restaurantmenu_id = Column(Integer, ForeignKey("restaurant.id"))
-	restaurant = relationship(Restaurant)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
-################
-## Config End
-################
-engine = create_engine("sqlite:///restaurantmenu.db")
+#######################
+# Config End
+#######################
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+
 Base.metadata.create_all(engine)
